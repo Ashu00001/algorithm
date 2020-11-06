ffd7d8da726d363474e03510ac328d3a96d84f72
Ashutosh Gupta <43481150+Ashu00001@users.noreply.github.com>
2020-11-06 21:46:46 +0530

    Create power of no using recursion.cpp

---
+++ power of no using recursion.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int pow(int x,int n)
+{
+	if(n==0)
+	{
+		return 1;
+	}
+	else if(n%2==0)
+	{
+		return pow(x,n/2)*pow(x,n/2);
+	}
+	
+	else{
+		return x*pow(x,n-1);
+	}
+}
+int main()
+{
+	cout<<pow(5,3);
+}
